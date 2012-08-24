a8d9e3b05d80c93ecf908c2cc09a2f727922e35c
Manoj Kumar <mkumar@crri.co.in>
2012-08-24 15:36:52 +0530

    n parantheses problem solve inefficiently

---
+++ n_parentheses.rb
@@ -0,0 +1,58 @@
+# =================== SAMPLE INPUTS & ITS RESULTS
+#
+#    1 => ()
+#
+#    2 => ()() | (())
+#
+#    3 =>
+#
+#    ()()()
+#
+#    ()(())	(())()
+#
+#    ((()))
+#
+#    4 =>
+#
+#    ()()()()
+#
+#    ()()(())		()(())()		(())()()		(())(())
+#
+#    ((()))()		()((()))
+#
+#    (((())))
+
+
+print "\nEnter a number(n): "
+n = gets().to_i
+
+array = (0..n).to_a.repeated_combination(n).reject do |i|
+  i.delete(0);
+  i.inject(:+) != n
+end.collect{|i| i.permutation.to_a.uniq }.flatten(1)
+
+def n_paranthesis_string(n)
+  s = ''
+  n.times do
+    s << '('
+  end
+
+  n.times do
+    s << ')'
+  end
+
+  s
+end
+
+
+
+result = array.collect do |ar|
+	s = ''
+	ar.each do |i|
+		s  << n_paranthesis_string(i)
+	end
+s
+end.reverse.join(' | ')
+
+puts "\nResult"
+puts result
