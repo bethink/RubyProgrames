a76f33280b0ca8a8fecba3f3eba89aca2a8144df
Manoj Kumar <mkumar@crri.co.in>
2012-08-29 11:13:31 +0530

    variable names changed

--- n_parentheses.rb
+++ n_parentheses.rb
@@ -22,38 +22,37 @@
 #
 #    (((())))
 
-
 print "\nEnter a number(n): "
 n = gets().to_i
 
-array = (0..n).to_a.repeated_combination(n).reject do |i|
-  i.delete(0);
-  i.inject(:+) != n
+
+# === Generate all the combination of array of integers. 
+# === Each array sum in equal to 'n'
+n_permutations = (0..n).to_a.repeated_combination(n).reject do |i|
+  is_equal_sum_n = ( i.inject(:+) == n )
+  i.delete(0) if is_equal_sum_n
+  !is_equal_sum_n
 end.collect{|i| i.permutation.to_a.uniq }.flatten(1)
 
+
+# ======== n_paranthesis_string(3) => '((()))'
+# ======== n_paranthesis_string(2) => '(())'
+# ======== n_paranthesis_string(1) => '()'
 def n_paranthesis_string(n)
-  s = ''
-  n.times do
-    s << '('
-  end
-
-  n.times do
-    s << ')'
-  end
-
-  s
+  paranthesis_string = ''
+  n.times{ paranthesis_string << '(' }
+  n.times{ paranthesis_string << ')' }
+  paranthesis_string
 end
 
+resultant_string = n_permutations.collect do |permutation_i|
+  result = ''
+  permutation_i.each do |i|
+	result << n_paranthesis_string(i)
+  end
+  result
+end.reverse.join(' | ')
 
-
-result = array.collect do |ar|
-	s = ''
-	ar.each do |i|
-		s  << n_paranthesis_string(i)
-	end
-s
-end.reverse.join(' , ')
-
-puts "\nResult:"
-puts result
 puts
+puts resultant_string
+puts 
